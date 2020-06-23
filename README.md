075efcb314562b684e8b1372dee36e3960e14fb7
Nicola Mosco <nicola.mosco@gmail.com>
2020-06-24 00:21:41 +0200

    Initial files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sphinx Doc Preview
+
+Preview your Sphinx generated documentation and keep view in sync with source.
+
+It uses `sphinx-build`, so you need to have the Sphinx generator in your `PATH`.
---
+++ spec/sphinx-doc-preview-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SphinxDocPreviewView from '../lib/sphinx-doc-preview-view';
+
+describe('SphinxDocPreviewView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
